ad37087fd87c773bf4d20b4c0e68812a9c2baf47
James <35617601+jliby@users.noreply.github.com>
2022-06-12 21:52:58 -0400

    NEW COMMIT

---
+++ orb-exercise/src/scripts/tweet.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+twurl -c $CONSUMER_KEY \
+    -s $CONSUMER_SECRET \
+    -a $ACCESS_TOKEN \
+    -S $ACCESS_SECRET\
+    -d "status=Build completed. CHECK STATUS: https://app.circleci.com/" /1.1/statuses/update.json
